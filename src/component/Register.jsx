f88285221e3cc90565d4dd5ff8f2880e7ca89773
ToufiqLizon <lijonlijon30@gmail.com>
2026-05-05 16:52:27 +0600

    update

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -3,41 +3,23 @@ import { Link } from 'react-router-dom'
 
 const Register = () => {
 
-     const [username,setUsername]=useState("")
-     const [massege,setMassege]=useState("")
-      const [email,setEmail]=useState("")
-      const [password,setPassword]=useState("")
     
-      const handleRegister=async(e)=>{
-    e.preventDefault()
-    const data={
-
-          username,
-      email,
-      password,
-    
-    }
-    console.log(data);
-    
-      }
   return (
        <section className='flex items-center justify-center h-screen'>
         <div className='max-w-sm border shadow-2xl bg-white mx-auto p-8'>
           <h2 className='text-2xl font-semibold pt-5'>Please Register</h2>
-          <form onSubmit={handleRegister} className='pt-8 flex  flex-col gap-y-2'>
+          <form  className='pt-8 flex  flex-col gap-y-2'>
 
- <input onChange={(e)=>setUsername(e.target.value)} type="text" name='username' id='username' placeholder='Enter Username' required
+ <input  type="text" name='username' id='username' placeholder='Enter Username' required
              className="w-full bg-gray-100 focus:outline-none px-5 py-3"  />
 
 
-             <input onChange={(e)=>setEmail(e.target.value)} type="email" name='email' id='email' placeholder='Enter email' required
+             <input  type="email" name='email' id='email' placeholder='Enter email' required
              className="w-full bg-gray-100 focus:outline-none px-5 py-3"  />
-             <input onChange={(e)=>setPassword(e.target.value)}  type="password" name='password' id='password' placeholder='Enter password' required
+             <input   type="password" name='password' id='password' placeholder='Enter password' required
              className="w-full bg-gray-100 focus:outline-none px-5 py-3"  />
             
-           {
-            massege && <p className='text-red-500'>{massege}</p>
-           }
+          
 
            <button  className="inline-block py-2 px-5 mt-5 bg-red-500 hover:bg-blue-500 transition rounded-2xl text-white text-sm sm:text-base md:text-lg">Register</button>
           </form>
